a889142ff8b5b8ce3e3fac8daaa3a08c3dd65720
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2021-01-23 00:16:56 -0600

    Add method to load sprite from aseprite file

--- core/src/main/java/zendo/games/zenlib/assets/Content.java
+++ core/src/main/java/zendo/games/zenlib/assets/Content.java
@@ -28,108 +28,13 @@ public class Content {
         textures = new ArrayList<>();
         sprites = new ArrayList<>();
 
-        // TODO: add atlas and sprite packing step
+        // TODO: add a sprite packing step that generates SpriteInfo objects that contain data needed to create actual sprites
+        //       change loadSprite to create Sprite objects based on data in a SpriteInfo param
+        //       that references TextureRegions in a packed atlas and other aseprite metadata used to create sprites
 
-        // todo - dispose Aseprite Pixmap images once done loading them here
-
-        // load player sprite
-        var player = new Sprite();
-        {
-            // load ase file
-            var ase = new Aseprite("sprites/player.ase");
-
-            // extract properties from aseprite
-            player.name = "player";
-            player.origin.set(0, 0);
-            if (ase.slices.size() > 0 && ase.slices.get(0).has_pivot) {
-                var slice = ase.slices.get(0);
-                // flip slice pivot point to be y-up to match in-game reference with in-aseprite pivot point
-                player.origin.set(slice.pivot.x, slice.pivot.y - slice.height);
-            }
-
-            // build animation for each tag
-            for (var tag : ase.tags) {
-                var num_frames = tag.to - tag.from + 1;
-                var anim_frames = new Sprite.Frame[num_frames];
-                for (int i = 0; i < num_frames; i++) {
-                    int frame_index = tag.from + i;
-                    var frame = ase.frames.get(frame_index);
-                    var frame_texture = new Texture(frame.image);
-                    var sprite_frame = new Sprite.Frame(new TextureRegion(frame_texture), frame.duration / 1000f);
-                    anim_frames[i] = sprite_frame;
-                    textures.add(frame_texture);
-                }
-                var anim = new Sprite.Anim(tag.name, anim_frames);
-                player.animations.add(anim);
-            }
-        }
-        sprites.add(player);
-
-        // load blob sprite
-        var blob = new Sprite();
-        {
-            // load ase file
-            var ase = new Aseprite("sprites/blob.ase");
-
-            // extract properties from aseprite
-            blob.name = "blob";
-            blob.origin.set(0, 0);
-            if (ase.slices.size() > 0 && ase.slices.get(0).has_pivot) {
-                var slice = ase.slices.get(0);
-                // flip slice pivot point to be y-up to match in-game reference with in-aseprite pivot point
-                blob.origin.set(slice.pivot.x, slice.pivot.y - slice.height);
-            }
-
-            // build animation for each tag
-            for (var tag : ase.tags) {
-                var num_frames = tag.to - tag.from + 1;
-                var anim_frames = new Sprite.Frame[num_frames];
-                for (int i = 0; i < num_frames; i++) {
-                    int frame_index = tag.from + i;
-                    var frame = ase.frames.get(frame_index);
-                    var frame_texture = new Texture(frame.image);
-                    var sprite_frame = new Sprite.Frame(new TextureRegion(frame_texture), frame.duration / 1000f);
-                    anim_frames[i] = sprite_frame;
-                    textures.add(frame_texture);
-                }
-                var anim = new Sprite.Anim(tag.name, anim_frames);
-                blob.animations.add(anim);
-            }
-        }
-        sprites.add(blob);
-
-        // load pop sprite
-        var pop = new Sprite();
-        {
-            // load ase file
-            var ase = new Aseprite("sprites/pop.ase");
-
-            // extract properties from aseprite
-            pop.name = "pop";
-            pop.origin.set(0, 0);
-            if (ase.slices.size() > 0 && ase.slices.get(0).has_pivot) {
-                var slice = ase.slices.get(0);
-                // flip slice pivot point to be y-up to match in-game reference with in-aseprite pivot point
-                pop.origin.set(slice.pivot.x, slice.pivot.y - slice.height);
-            }
-
-            // build animation for each tag
-            for (var tag : ase.tags) {
-                var num_frames = tag.to - tag.from + 1;
-                var anim_frames = new Sprite.Frame[num_frames];
-                for (int i = 0; i < num_frames; i++) {
-                    int frame_index = tag.from + i;
-                    var frame = ase.frames.get(frame_index);
-                    var frame_texture = new Texture(frame.image);
-                    var sprite_frame = new Sprite.Frame(new TextureRegion(frame_texture), frame.duration / 1000f);
-                    anim_frames[i] = sprite_frame;
-                    textures.add(frame_texture);
-                }
-                var anim = new Sprite.Anim(tag.name, anim_frames);
-                pop.animations.add(anim);
-            }
-        }
-        sprites.add(pop);
+        loadSprite("player", "sprites/player.ase");
+        loadSprite("blob", "sprites/blob.ase");
+        loadSprite("pop", "sprites/pop.ase");
     }
 
     public static void unload() {
@@ -150,6 +55,52 @@ public class Content {
             }
         }
         return null;
+    }
+
+    private static Sprite loadSprite(String name, String path) {
+        var sprite = new Sprite();
+        {
+            // load ase file
+            var ase = new Aseprite(path);
+
+            // extract properties from aseprite
+            sprite.name = name;
+            sprite.origin.set(0, 0);
+            if (ase.slices.size() > 0 && ase.slices.get(0).has_pivot) {
+                var slice = ase.slices.get(0);
+                // flip slice pivot point to be y-up to match in-game reference with in-aseprite pivot point
+                sprite.origin.set(slice.pivot.x, slice.pivot.y - slice.height);
+            }
+
+            // build animation for each tag
+            for (var tag : ase.tags) {
+                var num_frames = tag.to - tag.from + 1;
+                var anim_frames = new Sprite.Frame[num_frames];
+                for (int i = 0; i < num_frames; i++) {
+                    int frame_index = tag.from + i;
+                    var frame = ase.frames.get(frame_index);
+                    var frame_texture = new Texture(frame.image);
+                    var sprite_frame = new Sprite.Frame(new TextureRegion(frame_texture), frame.duration / 1000f);
+                    anim_frames[i] = sprite_frame;
+                    textures.add(frame_texture);
+                }
+
+                var anim = new Sprite.Anim(tag.name, anim_frames);
+                sprite.animations.add(anim);
+            }
+
+            // dispose Aseprite Pixmap images now that they're loaded as Textures
+            for (var frame : ase.frames) {
+                for (var cel : frame.cels) {
+                    cel.image.dispose();
+                }
+                frame.image.dispose();
+            }
+        }
+        sprites.add(sprite);
+
+
+        return sprite;
     }
 
 }
